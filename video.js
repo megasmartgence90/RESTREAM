4763507b6b2fd686639f9a05fbca2a6905a5c863
MEGA-GROUP <uzagdasan@mail.ru>
2025-02-11 02:40:08 +0400

    Update video.js

--- video.js
+++ video.js
@@ -8,7 +8,7 @@
     width="640"
     height="360"
 >
-    <source src="http://YOUR_SERVER_ADDRESS/stream.m3u8" type="application/x-mpegURL">
+    <source src="http://http://185.129.1.242:80/aztv/stream.m3u8" type="application/x-mpegURL">
 </video>
 <script>
     var player = videojs('my-video');
